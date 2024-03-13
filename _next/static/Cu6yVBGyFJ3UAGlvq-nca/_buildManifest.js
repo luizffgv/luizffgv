82b93c259570f4ba5ff0655af8d0b8586f8d56f0
luizffgv <luizffgv@users.noreply.github.com>
2024-03-13 02:13:53 +0000

    deploy: 0ad84915c08e6d716b847fb00ada636b861bb416

---
+++ _next/static/Cu6yVBGyFJ3UAGlvq-nca/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[{has:[{type:"header",key:"Next-Url",value:"(?:\\/(.*))?[\\/#\\?]?"}],source:"/color-picker",destination:"/(.)color-picker"}],fallback:[]},"/_error":["static/chunks/pages/_error-9a890acb1e81c3fc.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/app/page-7fa8a2007d360e19.js
@@ -0,0 +1,161 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8157:function(e,a,t){Promise.resolve().then(t.bind(t,825))},825:function(e,a,t){"use strict";t.r(a),t.d(a,{default:function(){return x}});var i=t(7437),o=t(2265);let s=(0,o.createContext)({filter:e=>e,setFilter:()=>{}});function r(e,...a){let t=a.map(String);return t.push(""),e.map((e,a)=>e+t[a]).reduce((e,a)=>e+a)}let n=[{name:"Blink: Ultimate Teleportation",description:"Instalado por mais de 32 mil usu\xe1rios diferentes, Blink \xe9 um mod feito em Lua para o jogo Teardown.",details:r`
+      <p>
+        Blink: Ultimate Teleportation é um mod para
+        <a href="https://teardowngame.com/">Teardown</a> que te permite desafiar
+        a física e percorrer distâncias em um piscar de olhos.
+      </p>
+      <p>
+        Tipagens via
+        <a href="https://github.com/LuaLS/lua-language-server"
+          >lua-language-server</a
+        >
+        foram amplamente utilizadas.<br />
+        Embora eu não seja experiente em Lua e não seja fã de suas
+        particularidades, o mod rapidamente ficou em alta na Oficina Steam.
+      </p>
+    `,tags:["GitHub","Lua","Mod","Python"],url:"https://steamcommunity.com/sharedfiles/filedetails/?id=2875792342",repository:"https://github.com/luizffgv/blink-mod-teardown",image:"Blink Ultimate Teleportation"},{name:"codinStruct",description:"Uma organiza\xe7\xe3o feita como projeto de faculdade, visando ajudar estudantes a aprender C, JavaScript e Python.  Trabalhei principalmente na organiza\xe7\xe3o, no Sass e no conte\xfado de C.",details:r`
+      <p>
+        codinStruct é um projeto com o intuito de disponibilizar conteúdo para
+        auxiliar no aprendizado de C, JavaScript e Python.
+      </p>
+      <p>
+        Eu escrevi o conteúdo sobre C (2641 linhas no momento em que isso foi
+        escrito) e trabalhei nos estilos Sass utilizados nas páginas de
+        conteúdo, e parte do HTML e workflows.
+      </p>
+      <h3>Como funciona o projeto?</h3>
+      <p>A organização até o momento possui três repositórios públicos:</p>
+      <ul class="raiar-list-text">
+        <li>
+          <a href="https://github.com/codinStruct/codinStruct"
+            ><strong>codinStruct</strong></a
+          >&mdash;contém o código para criar uma imagem Docker para servir o
+          website.
+        </li>
+        <li>
+          <a href="https://github.com/codinStruct/codinStruct-Content"
+            ><strong>codinstruct-Content</strong></a
+          >&mdash;contém o conteúdo disponibilizado no website, com a licença CC
+          BY-SA 4.0.<br />
+          Esse repositório também hospeda o conteúdo diretamente via mdBook e
+          GitHub Pages, acessível
+          <a href="https://codinStruct.github.io/codinStruct-content/">aqui</a>.
+        </li>
+        <li>
+          <a href="https://github.com/codinStruct/md2html"
+            ><strong>md2html</strong></a
+          >&mdash;Contém um script Python para converter Markdown para HTML.
+        </li>
+      </ul>
+      <p>
+        Todo o conteúdo é escrito em Markdown, que é automaticamente processado
+        pelo servidor e se torna HTML que por sua vez é servido e estilizado com
+        Sass.
+      </p>
+    `,tags:["Bash","Docker","Express.js","GitHub","GitHub Actions","HTML","JavaScript","Markdown","mdBook","Node.js","Organiza\xe7\xe3o","Python","Sass","Site","TypeScript","Webpack"],url:"https://github.com/codinStruct",image:"codinStruct"},{name:"codinStruct Next",description:"Front-end em Next.js feito para o projeto codinStruct.",tags:["GitHub","GitHub Actions","HTML","Next.js","Node.js","React","TypeScript"],url:"https://codinstruct.luizf.dev",repository:"https://github.com/luizffgv/codinStruct-next"},{name:"ExpTL",description:"Biblioteca de C++ de uso geral, pretendendo complementar a biblioteca padr\xe3o.",tags:["Biblioteca","C++","C++20","Doxygen","GitHub","GitHub Actions"],license:"MIT License",url:"https://exptl.luizf.dev/modules.html",repository:"https://github.com/luizffgv/exptl"},{name:"Figment",description:"Uma classe em C++ que permite controle preciso da vida \xfatil de objetos de qualquer tipo. Semelhante a std::optional, mas n\xe3o cont\xe9m informa\xe7\xf5es sobre o objeto contido.",tags:["Biblioteca","C++","C++17","C++20","Doxygen","GitHub Actions","GitHub"],license:"Unlicense",url:"https://figment.luizf.dev",repository:"https://github.com/luizffgv/Figment"},{name:"Glimmer",description:"Um simples framework modular para bots de Discord baseado em discord.js.",tags:["Framework","Discord App","GitHub","GitHub Actions","JavaScript","Node.js","TypeScript"],license:"MIT License",repository:"https://github.com/luizffgv/Glimmer"},{name:"Globe",description:"Um site com um globo girat\xf3rio formado por pontos, te permitindo usar suas pr\xf3prias imagens como texturas.",tags:["GitHub Actions","GitHub","HTML","JavaScript","Node.js","Sass","Site","Three.js","TypeScript","Webpack"],details:r`
+      <p>
+        Globe utiliza Three.js para exibir um globo giratório de pontos com
+        efeitos de bloom e ondulação.
+      </p>
+      <p>
+        É possível escolher uma imagem para ser exibida no globo, mas ela deve
+        seguir um padrão específico.
+      </p>
+      <p>
+        A movimentação de cada ponto no globo é determinada via JavaScript, mas
+        é também factível implementar via shaders para melhor desempenho.
+      </p>
+    `,license:"MIT License",url:"https://globe.luizf.dev",repository:"https://github.com/luizffgv/globe",image:"Globe"},{name:"HTilesML",description:"Jogo experimental utilizando elementos HTML e transforma\xe7\xf5es CSS 3D.",details:r`
+      <p>
+        HTilesML é um pequeno experimento que serve como prova de conceito da
+        possibilidade de criar um jogo 3D sem canvas&mdash;utilizando apenas
+        elementos HTML.
+      </p>
+      <p>
+        O visual do jogo é feito utilizando ortoedros formados por faces
+        posicionadas com transformações CSS 3D.
+      </p>
+      <p>
+        Admitidamente não é um bom jogo sob nenhum padrão e realmente só deve
+        ser visto como experimento.
+      </p>
+    `,tags:["GitHub","HTML","JavaScript","Site"],license:"MIT License",url:"https://htilesml.luizf.dev",repository:"https://github.com/luizffgv/htilesml"},{name:"livro-c",description:"Livro virtual gratuito que pretende trazer uma fonte moderna, confi\xe1vel e completa para o aprendizado de C.",tags:["C","GitHub","Livro","mdBook"],license:"CC BY-SA 4.0",url:"https://luizffgv.github.io/livro-c/",repository:"https://github.com/luizffgv/livro-c"},{name:"Modern C Sorting Algorithms",description:"Cole\xe7\xe3o de algoritmos de ordena\xe7\xe3o gen\xe9ricos utilizando o padr\xe3o C18 extensivamente, feita para facilitar meus trabalhos de C na faculdade.",details:r`
+      <p>Uma coleção de algoritmos de ordenação utilizando o padrão C18.</p>
+      <h3>Pontos notáveis</h3>
+      <ul class="raiar-list-text">
+        <li>
+          Uso de macros em definições e implementações de funções, reduzindo
+          drasticamente o número de linhas de código.
+        </li>
+        <li>
+          Combinação de <code>_Generic</code> e <code>static_assert</code> para
+          asseguração de interfaces.
+        </li>
+        <li>
+          Uso de <code>_Generic</code> para determinar automaticamente qual
+          função executar baseado nos tipos dos argumentos.
+        </li>
+      </ul>
+    `,tags:["Biblioteca","C","C18","GitHub"],license:"GNU General Public License v3.0",repository:"https://github.com/luizffgv/modern-c-sorting-algorithms"},{name:"Raiar",description:"Um pequeno framework web front-end fornecendo web components JS e estilos Sass configur\xe1veis.",details:r`
+      Raiar é um pequeno framework de Sass e web components JS, criado para meus
+      projetos com a intenção de acelerar o desenvolvimento e manter
+      consistência entre eles.<br /><br />Raiar não é muito portável pois
+      utiliza várias funcionalidades relativamente novas no CSS, portanto não é
+      recomendado para todos os tipos de projetos.
+      <h3>Funcionalidades</h3>
+      <ul class="raiar-list-text">
+        <li>
+          <strong>
+            Raiar utiliza tanto variáveis Sass quanto variáveis CSS</strong
+          >, permitindo alterações nos estilos via JavaScript mesmo após a
+          compilação.
+        </li>
+        <li>
+          <strong
+            >Raiar fornece um equilíbrio entre shadow DOM e light DOM</strong
+          >&mdash;web components só são utilizados quando há a necessidade de
+          executar JavaScript no funcionamento de um elemento.
+        </li>
+        <li>
+          <strong>Raiar infere o significado dos elementos por contexto</strong
+          >&mdash;o uso das pseudo-classes <code>:has</code> e
+          <code>:is</code> permite que elementos sejam estilizados corretamente
+          sem o uso classes.
+        </li>
+      </ul>
+    `,tags:["GitHub","HTML","Framework","JavaScript","Sass"],license:"MIT License",repository:"https://github.com/luizffgv/raiar"},{name:"Separador de s\xedlabas",description:"Um site feito em Next.js que divide um texto em s\xedlabas utilizando um algoritmo que criei.",tags:["GitHub","GitHub Actions","HTML","JavaScript","Next.js","Node.js","React","Site","TypeScript"],license:"MIT License",url:"https://silabas.luizf.dev/",repository:"https://github.com/luizffgv/silabas-website"},{name:"Shader Site",description:"Um site que te ajuda a encontrar o shaderpack de Minecraft ideal para voc\xea.",tags:["GitHub","GitHub Actions","HTML","JavaScript","Lit","Node.js","Sass","Site","TypeScript","Webpack"],license:"MIT License",repository:"https://github.com/luizffgv/shader-site",url:"https://shadersite.luizf.dev",image:"Shader Site"},{name:"SoR-pt-BR",description:"Tradu\xe7\xe3o n\xe3o oficial para o jogo Streets of Rogue, com melhorias—algumas subjetivas—em rela\xe7\xe3o \xe0 vers\xe3o oficial.",tags:["GitHub","Python"],license:"Unlicense",repository:"https://github.com/luizffgv/SoR-pt-BR"},{name:"Spirit Box",description:"Bot para Discord que gera um di\xe1rio de anota\xe7\xf5es do jogo Phasmophobia, para auxiliar na identifica\xe7\xe3o de fantasmas e no compartilhamento de pistas com sua equipe.",details:r`
+      <p>
+        Bot para Discord com o intuito de compartilhar pistas e facilitar a
+        identificação de fantasmas no jogo Phasmophobia.
+      </p>
+      <p>
+        Spirit Box utiliza TypeScript extensivamente e possui um
+        <a
+          href="https://github.com/luizffgv/Spirit-Box/blob/b0734c85de03127fca33b9408b4f2b229366df6d/src/commands/journal.ts#L148-L202"
+          >algoritmo</a
+        >
+        <strong>compacto</strong> e <strong>genérico</strong> para a
+        identificação de fantasmas, com um modelo que o torna aplicável, sem
+        alterações, a todos os fantasmas e dificuldades.
+      </p>
+      <h3>Passos do algoritmo</h3>
+      <ul class="raiar-list-text">
+        <li>
+          Descartar fantasmas que não possuem alguma das evidências encontradas,
+          ignorando evidências falsas.
+        </li>
+        <li>
+          Descartar fantasmas que apresentam mais evidências do que a
+          dificuldade permite, ignorando evidências falsas.
+        </li>
+        <li>
+          Descartar fantasmas que possuem mais evidências ausentes do que
+          evidências desabilitadas pela dificuldade.
+        </li>
+        <li>
+          Descartar fantasmas cuja evidência garantida está ausente ou
+          inobtível.
+        </li>
+        <li>Descartar fantasmas cuja evidência falsa está ausente.</li>
+      </ul>
+    `,tags:["Discord App","GitHub","JavaScript","Node.js","TypeScript"],license:"MIT License",repository:"https://github.com/luizffgv/spirit-box",image:"Spirit Box"},{name:"TS Conversions",description:"Biblioteca e pacote NPM fornecendo utilit\xe1rios para realizar convers\xf5es seguras ou inseguras e diminuir c\xf3digo boilerplate no TypeScript. Tamb\xe9m \xe9 utilizada nesse site.",tags:["Biblioteca","GitHub Actions","GitHub","JavaScript","Node.js","Pacote NPM","TypeScript"],license:"MIT License",url:"https://www.npmjs.com/package/@luizffgv/ts-conversions",repository:"https://github.com/luizffgv/ts-conversions"},{name:"Viewport-Based Scaling",description:"Site que gera CSS para definir o valor de uma propriedade baseado na largura do viewport, realizando interpola\xe7\xe3o linear entre v\xe1rios pares de pontos.",tags:["GitHub","GitHub Actions","HTML","JavaScript","Node.js","Site","TypeScript","Webpack"],license:"MIT License",url:"https://vbs.luizf.dev",repository:"https://github.com/luizffgv/viewport-based-rescaling"}];var l=t(9704),c=t.n(l);let d=[...n.map(e=>e.tags).reduce((e,a)=>{for(let t of a)e.add(t);return e},new Set)].sort();function p(e,a){var t;return new Set((null!==(t=e.tags)&&void 0!==t?t:[]).filter(e=>a.has(e)))}function u(){let{setFilter:e}=(0,o.useContext)(s),[a,t]=(0,o.useState)("best-match"),[r,n]=(0,o.useState)({value:new Set});return(0,o.useEffect)(()=>{e(e=>{let t=[...e].sort((e,a)=>p(a,r.value).size-p(e,r.value).size);return r.value.size>0&&(t=t.filter(e=>p(e,r.value).size>0)),"every"==a&&(t=t.filter(e=>{for(let t of r.value){var a;if(!(null===(a=e.tags)||void 0===a?void 0:a.includes(t)))return!1}return!0})),t})},[a,r,e]),(0,i.jsx)("div",{id:"filters-accordion",className:c().container,children:(0,i.jsxs)("details",{children:[(0,i.jsx)("summary",{children:"Filtros"}),(0,i.jsxs)("div",{className:"raiar flex-row flex-wrap gap justify-content-center",children:[(0,i.jsxs)("fieldset",{id:"tags",className:"".concat(c().tags," raiar flex-row flex-wrap gap"),onChange:e=>{if(!(e.target instanceof HTMLInputElement)){console.error("event.target is not an HTMLInputElement");return}e.target.checked?r.value.add(e.target.value):r.value.delete(e.target.value),n({value:r.value})},children:[(0,i.jsx)("legend",{children:"Selecione tags"}),d.map(e=>(0,i.jsxs)("label",{children:[e," ",(0,i.jsx)("input",{type:"checkbox",value:e})]},e))]}),(0,i.jsxs)("fieldset",{id:"project-filter-modes",onChange:e=>{if(!(e.target instanceof HTMLInputElement)){console.error("event.target is not an HTMLInputElement");return}t(e.target.value)},children:[(0,i.jsx)("legend",{children:"L\xf3gica de filtragem"}),(0,i.jsxs)("label",{children:[(0,i.jsx)("input",{type:"radio",name:"project-filter-mode",value:"best-match",defaultChecked:!0}),"Melhor correspond\xeancia"]}),(0,i.jsxs)("label",{children:[(0,i.jsx)("input",{type:"radio",name:"project-filter-mode",value:"every"}),"Possui todas categorias selecionadas"]})]})]})]})})}var m=t(4627),g=t.n(m),f=t(8792);function h(e){let{project:a}=e;return(0,i.jsxs)("div",{className:"".concat(g().project," raiar-card raiar align-items-center flex-col gap justify-content-between"),role:"group","aria-label":a.name,children:[(0,i.jsxs)("div",{className:"".concat(g()["title-container"]," raiar flex-row gap flex-wrap align-items-center justify-content-center"),style:{minHeight:"48px"},children:[null==a.image?(0,i.jsx)(i.Fragment,{}):(0,i.jsx)("img",{className:g().image,src:"/project-images/".concat(a.image,".webp"),alt:a.name}),(0,i.jsxs)("div",{className:"raiar flex-row gap flex-wrap align-items-baseline",children:[(0,i.jsxs)("h3",{children:[a.name," ",a.wip?"(em progresso)":""]}),(0,i.jsx)("small",{className:g().license,children:a.license})]})]}),(0,i.jsx)("p",{children:a.description}),(0,i.jsxs)("div",{className:"raiar flex-row gap flex-wrap justify-content-center",children:[null==a.url?(0,i.jsx)(i.Fragment,{}):(0,i.jsxs)("a",{className:"".concat(g()["button-view"]," button"),href:a.url,children:[(0,i.jsx)("span",{className:"material-symbols-outlined","aria-hidden":"true",children:"visibility"}),"Visualizar"]}),null==a.repository?(0,i.jsx)(i.Fragment,{}):(0,i.jsxs)("a",{className:"button",href:a.repository,"aria-label":"Reposit\xf3rio",children:[(0,i.jsx)("span",{className:"material-symbols-outlined","aria-hidden":"true",children:"code"}),"Reposit\xf3rio"]}),null==a.details?(0,i.jsx)(i.Fragment,{}):(0,i.jsxs)(f.default,{href:"projetos/".concat(encodeURIComponent(a.name)),className:"".concat(g()["button-details"]," button"),children:[(0,i.jsx)("span",{className:"material-symbols-outlined","aria-hidden":"true",children:"add"}),"Detalhes"]})]})]})}var v=t(5502),b=t.n(v);function j(){let{filter:e}=(0,o.useContext)(s),a=(0,o.useMemo)(()=>e(n),[e]);return(0,i.jsx)("ul",{className:"".concat(b().list," no-list-style raiar flex-row flex-wrap gap-2x justify-content-center"),children:a.map(e=>(0,i.jsx)("li",{className:"display-contents",children:(0,i.jsx)(h,{project:e})},e.name))})}function x(){let[e,a]=(0,o.useState)(()=>e=>e),t=(0,o.useCallback)(e=>a(()=>e),[]),r=(0,o.useRef)(null);return(0,o.useLayoutEffect)(()=>{if(null==r){console.error("projectsListRef is null");return}new Animation(new KeyframeEffect(r.current,[{opacity:0},{}],{duration:500,easing:"ease"})).play()},[e]),(0,i.jsx)("section",{className:"raiar flex-col gap-2x align-center","aria-labelledby":"heading-projects",children:(0,i.jsxs)(s.Provider,{value:{filter:e,setFilter:t},children:[(0,i.jsxs)("div",{id:"projects-title-bar",className:"raiar flex-row gap flex-wrap justify-content-center",children:[(0,i.jsx)("h2",{id:"heading-projects",children:"Meus projetos"}),(0,i.jsx)(u,{})]}),(0,i.jsx)("div",{ref:r,children:(0,i.jsx)(j,{})})]})})}},9704:function(e){e.exports={container:"project-filter-selector_container__BE1_m","fade-in-2":"project-filter-selector_fade-in-2__fSgxN",tags:"project-filter-selector_tags__gJ3Rg","fade-in":"project-filter-selector_fade-in__QiqdL"}},4627:function(e){e.exports={project:"project-preview_project__KjAlH","slide-in":"project-preview_slide-in__95pvX","title-container":"project-preview_title-container__4S_EM",image:"project-preview_image__CoIkg",license:"project-preview_license__jGF42","button-details":"project-preview_button-details__j8Zab",wiggle:"project-preview_wiggle__RUxYV","button-view":"project-preview_button-view__beZ_q","eye-blink":"project-preview_eye-blink__h6Z3f"}},5502:function(e){e.exports={list:"projects-list_list__ag9er"}}},function(e){e.O(0,[792,971,69,744],function(){return e(e.s=8157)}),_N_E=e.O()}]);
