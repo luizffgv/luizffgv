dc11148641037bae787527045c6c2c92d57e0f11
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-14 00:14:45 -0300

    Make project details a component

--- src/app/projetos/[projectName]/page.tsx
+++ src/app/projetos/[projectName]/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import projects, { ProjectRaw } from "projects-list";
 import styles from "./page.module.scss";
-import "./page.global.scss";
+import Project from "@/app/_components/project";
 
 export function generateStaticParams() {
   return projects.map((project) => ({
@@ -44,31 +44,7 @@ export default function Page({
 
   return (
     <main className="raiar flex-col align-items-center">
-      <div className="raiar-card width-fit">
-        {project.image == null ? (
-          <></>
-        ) : (
-          <div className="raiar flex-row justify-content-center">
-            <img
-              className={styles.image}
-              src={`/project-images/${project.image}.webp`}
-              alt={project.name}
-            ></img>
-          </div>
-        )}
-        <div className="content">
-          <h1>{project.name}</h1>
-          <p>{project.description}</p>
-          {project.details == null ? (
-            <></>
-          ) : (
-            <>
-              <h2>Detalhes</h2>
-              <div dangerouslySetInnerHTML={{ __html: project.details }}></div>
-            </>
-          )}
-        </div>
-      </div>
+      <Project project={project}></Project>
     </main>
   );
 }
