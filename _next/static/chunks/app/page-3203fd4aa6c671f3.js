200dbe28d657961920780d30784a88d5f619a73f
luizffgv <luizffgv@users.noreply.github.com>
2024-03-15 06:15:01 +0000

    deploy: 8a8343a84cd1c59a5592bddab1184bdb2b8aa04b

---
+++ _next/static/chunks/app/page-3203fd4aa6c671f3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{2995:function(e,n,s){Promise.resolve().then(s.t.bind(s,5250,23)),Promise.resolve().then(s.bind(s,2529)),Promise.resolve().then(s.t.bind(s,6977,23)),Promise.resolve().then(s.bind(s,9667))}},function(e){e.O(0,[792,667,971,69,744],function(){return e(e.s=2995)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/projetos/page-40f9ed642374b222.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[725],{8157:function(n,e,u){Promise.resolve().then(u.bind(u,9667))}},function(n){n.O(0,[792,667,971,69,744],function(){return n(n.s=8157)}),_N_E=n.O()}]);
