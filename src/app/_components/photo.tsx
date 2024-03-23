7e03102d41825e175c36491baeb8d2298c1292a0
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-23 16:35:21 -0300

    Add hover effect to photo

--- src/app/_components/photo.tsx
+++ src/app/_components/photo.tsx
@@ -1,7 +1,24 @@
 export default function Photo() {
   return (
     <div className="jump-in h-48 w-48 overflow-hidden rounded-full border-4 border-primary bg-bg-close drop-shadow-[0_0_15px_theme(colors.primary)] dark:bg-bg-close-dark">
-      <img src="/my-photo.webp" alt="Minha foto"></img>
+      <img
+        className="peer transition-transform hover:scale-110"
+        src="/my-photo-no-bg.webp"
+        alt="Minha foto"
+        aria-hidden
+      ></img>
+      {/* We duplicate the image so the blur effect doesn´t have a visible border */}
+      <img
+        className="absolute inset-0 z-[-1]"
+        src="/my-photo.webp"
+        alt="Minha foto"
+        aria-hidden
+      ></img>
+      <img
+        className="absolute inset-0 z-[-1] transition peer-hover:blur-sm"
+        src="/my-photo.webp"
+        alt="Minha foto"
+      ></img>
     </div>
   );
 }
