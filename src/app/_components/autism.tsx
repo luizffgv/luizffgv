79a526e953e95fc4405ab0f755147319091d7205
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-24 16:23:56 -0300

    Improve SR pause in autism section

--- src/app/_components/autism.tsx
+++ src/app/_components/autism.tsx
@@ -5,32 +5,34 @@ export default function Autism() {
     <TitledSection
       level={2}
       title={
-        <>
-          Pra deixar claro, eu tenho {/* Rainbow gradient */}
-          <strong className="color-in inline-block">
-            <span className="animate-[pulse_1s_infinite] text-red-500 drop-shadow-[0_0_10px_currentcolor]">
-              a
-            </span>
-            <span className="animate-[pulse_1s_100ms_infinite] text-orange-500 drop-shadow-[0_0_10px_currentcolor]">
-              u
-            </span>
-            <span className="animate-[pulse_1s_200ms_infinite] text-yellow-500 drop-shadow-[0_0_10px_currentcolor]">
-              t
-            </span>
-            <span className="animate-[pulse_1s_300ms_infinite] text-green-500 drop-shadow-[0_0_10px_currentcolor]">
-              i
-            </span>
-            <span className="animate-[pulse_1s_400ms_infinite] text-blue-500 drop-shadow-[0_0_10px_currentcolor]">
-              s
-            </span>
-            <span className="animate-[pulse_1s_500ms_infinite] text-indigo-500 drop-shadow-[0_0_10px_currentcolor]">
-              m
-            </span>
-            <span className="animate-[pulse_1s_600ms_infinite] text-purple-500 drop-shadow-[0_0_10px_currentcolor]">
-              o
-            </span>
-          </strong>
-        </>
+        <span aria-label="Pra deixar claro, eu tenho autismo">
+          <span aria-hidden>
+            Pra deixar claro, eu tenho {/* Rainbow gradient */}
+            <strong className="color-in inline-block">
+              <span className="animate-[pulse_1s_infinite] text-red-500 drop-shadow-[0_0_10px_currentcolor]">
+                a
+              </span>
+              <span className="animate-[pulse_1s_100ms_infinite] text-orange-500 drop-shadow-[0_0_10px_currentcolor]">
+                u
+              </span>
+              <span className="animate-[pulse_1s_200ms_infinite] text-yellow-500 drop-shadow-[0_0_10px_currentcolor]">
+                t
+              </span>
+              <span className="animate-[pulse_1s_300ms_infinite] text-green-500 drop-shadow-[0_0_10px_currentcolor]">
+                i
+              </span>
+              <span className="animate-[pulse_1s_400ms_infinite] text-blue-500 drop-shadow-[0_0_10px_currentcolor]">
+                s
+              </span>
+              <span className="animate-[pulse_1s_500ms_infinite] text-indigo-500 drop-shadow-[0_0_10px_currentcolor]">
+                m
+              </span>
+              <span className="animate-[pulse_1s_600ms_infinite] text-purple-500 drop-shadow-[0_0_10px_currentcolor]">
+                o
+              </span>
+            </strong>
+          </span>
+        </span>
       }
     >
       <p className="text-center">Você não tem preconceito, não é?</p>
