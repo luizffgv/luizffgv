9090d27977cb432f257b7ee4e933442a091cbc02
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-24 18:23:42 -0300

    Change button glow style

--- src/app/_components/radio-button.tsx
+++ src/app/_components/radio-button.tsx
@@ -19,9 +19,9 @@ export default function RadioButton({
   onChange,
 }: Props) {
   return (
-    <label className="relative inline-block rounded-lg border border-fg/5 bg-bg-close px-4 py-2 text-center font-bold shadow-sm transition-all hover:brightness-110 has-[:checked]:border-transparent has-[:checked]:bg-primary has-[:checked]:text-fg-on-primary has-[:checked]:shadow-[0_0_10px_theme(colors.primary)] dark:border-fg-dark/10 dark:bg-bg-close-dark dark:has-[:checked]:bg-primary">
+    <label className="relative inline-block rounded-lg border border-fg/5 bg-bg-close px-4 py-2 text-center font-bold shadow-sm transition-all has-[:checked]:border-transparent has-[:checked]:bg-primary has-[:checked]:text-fg-on-primary has-[:checked]:shadow-[0_0_50px_theme(colors.primary_/_25%)] has-[:checked]:[text-shadow:_0_0_15px_currentcolor] has-[:not(:checked)]:hover:brightness-110 dark:border-fg-dark/10 dark:bg-bg-close-dark dark:has-[:checked]:bg-primary">
       <input
-        className="absolute inset-0 cursor-pointer appearance-none rounded-lg"
+        className="absolute inset-0 cursor-pointer appearance-none rounded-lg checked:cursor-default"
         type="radio"
         name={name}
         value={value}
