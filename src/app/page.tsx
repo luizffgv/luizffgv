eb43ca378d5d99c5b7804b20a404eadbac63d100
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-03-26 16:15:38 -0300

    Fix gradient bg pushing siblings down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
       <div className="color-in">
         <Hero></Hero>
       </div>
-      <div className="bg-bg-close transition-colors dark:bg-bg-close-dark">
+      <div className="grid bg-bg-close transition-colors [grid-template-areas:'stack'] dark:bg-bg-close-dark">
         {/* Gradient background */}
-        <div className="sticky top-0 h-screen max-h-full w-full [background:radial-gradient(circle_at_bottom,_theme(colors.primary/20%),_transparent)] dark:[background:radial-gradient(circle_at_top,_theme(colors.primary/15%),_transparent)]"></div>
+        <div className="sticky top-0 h-screen max-h-full w-full [background:radial-gradient(circle_at_bottom,_theme(colors.primary/20%),_transparent)] [grid-area:stack] dark:[background:radial-gradient(circle_at_top,_theme(colors.primary/15%),_transparent)]"></div>
 
-        <div className="flex flex-col items-center gap-64 px-4 pb-[50vh] pt-8">
+        <div className="flex flex-col items-center gap-64 px-4 pb-[50vh] pt-8 [grid-area:stack]">
           <ReactProjects></ReactProjects>
           <div className="appear">
             <NotOnlyReact></NotOnlyReact>
