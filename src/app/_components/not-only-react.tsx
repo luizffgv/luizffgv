6f1cbc671277366d2cd55120a27ce4f81b83b682
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-04-01 17:21:57 -0300

    Add tool categories

--- src/app/_components/not-only-react.tsx
+++ src/app/_components/not-only-react.tsx
@@ -1,4 +1,5 @@
 import Button from "./button";
+import GlowingText from "./glowing-text";
 import TitledSection from "./titled-section";
 import Tools from "./tools";
 
@@ -6,13 +7,37 @@ export default function NotOnlyReact() {
   return (
     <>
       <TitledSection level={2} title="Mas eu não sei apenas React">
-        <div className="flex flex-col items-center gap-8">
-          <div className="flex flex-col gap-4">
-            <p className="text-center">Eu tenho conhecimento em:</p>
-            <div className="flex max-w-prose flex-col items-center gap-8">
-              <Tools></Tools>
-            </div>
-          </div>
+        <div className="flex flex-col items-center gap-16">
+          <TitledSection
+            level={3}
+            title={
+              <span>
+                Tenho conhecimento nestes <GlowingText>frameworks</GlowingText>
+              </span>
+            }
+          >
+            <Tools kind="framework"></Tools>
+          </TitledSection>
+          <TitledSection
+            level={3}
+            title={
+              <span>
+                Tenho conhecimento nestas <GlowingText>linguagens</GlowingText>
+              </span>
+            }
+          >
+            <Tools kind="language"></Tools>
+          </TitledSection>
+          <TitledSection
+            level={3}
+            title={
+              <span>
+                Tenho conhecimento nestas <GlowingText>ferramentas</GlowingText>
+              </span>
+            }
+          >
+            <Tools kind="tool"></Tools>
+          </TitledSection>
           <Button href="/projetos">
             <span
               className="material-symbols-outlined"
--- src/app/_components/tools.tsx
+++ src/app/_components/tools.tsx
@@ -1,65 +1,89 @@
-import Tool, { Props as ToolProps } from "./tool";
+import ToolComponent, { Props as ToolProps } from "./tool";
+
+/** Kinds of tools. */
+export type ToolKind = "language" | "framework" | "tool";
+
+/** Information about a tool. */
+export type ToolData = ToolProps & { kind: ToolKind };
 
 const TOOLS = [
   {
+    kind: "framework",
     name: "Next.js",
     image: "https://cdn.simpleicons.org/nextdotjs",
     details: "Para static exports",
   },
   {
+    kind: "framework",
     name: "Node.js",
     image: "https://cdn.simpleicons.org/nodedotjs",
     details: "E execução async",
   },
   {
+    kind: "language",
     name: "TypeScript",
     image: "https://cdn.simpleicons.org/typescript",
     details: "Com type safety e sem any",
   },
   {
+    kind: "language",
     name: "JavaScript puro",
     image: "https://cdn.simpleicons.org/javascript",
     details: "E componentes com shadow DOM",
   },
   {
+    kind: "framework",
     name: "Tailwind CSS",
     image: "https://cdn.simpleicons.org/tailwindcss",
     details: "Esse site usa Tailwind",
   },
   {
+    kind: "language",
     name: "Sass",
     color: "#c76494",
   },
   {
+    kind: "tool",
     name: "Webpack",
     color: "#1f7ac1",
   },
   {
+    kind: "tool",
     name: "JSDoc/TSDoc",
     details: "Com amor por documentar",
   },
   {
+    kind: "framework",
     name: "Jest",
     image: "https://cdn.simpleicons.org/jest",
     details: "Com React",
   },
   {
+    kind: "tool",
     name: "GitHub Actions",
     image: "https://cdn.simpleicons.org/githubactions",
   },
-] satisfies ToolProps[];
+] satisfies ToolData[];
 
-export default function Tools() {
+/** Properties for the {@link Tools} component. */
+export interface ToolsProps {
+  /** Filters tools by {@link ToolKind | kind}. */
+  kind?: ToolKind;
+}
+
+export default function Tools({ kind }: ToolsProps) {
   return (
     <ul
       className="flex flex-row flex-wrap justify-center gap-8"
       aria-label="Tools"
     >
-      {TOOLS.map((props) => (
-        <li key={props.name}>
-          <Tool {...props} />
-        </li>
-      ))}
+      {TOOLS.filter((tools) => kind == null || tools.kind === kind).map(
+        (props) => (
+          <li key={props.name}>
+            <ToolComponent {...props} />
+          </li>
+        ),
+      )}
     </ul>
   );
 }
