25d01b76deec889160654c9ee9b2eab9949a651d
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-04-17 19:37:45 -0300

    Add button to view my NPM packages

--- src/app/_components/my-github.tsx
+++ src/app/_components/my-github.tsx
@@ -1,4 +1,4 @@
-import { FolderGit2Icon } from "lucide-react";
+import { FolderGit2Icon, PackageIcon } from "lucide-react";
 import Button from "./button";
 import GlowingText from "./glowing-text";
 import TitledSection from "./titled-section";
@@ -13,14 +13,23 @@ export default function MyGitHub() {
         </>
       }
     >
-      <div className="flex flex-col items-center gap-4">
-        <p>Eu tenho vários projetos open source!</p>
-        <div>
-          <Button href="https://www.github.com/luizffgv/" target="_blank">
-            <FolderGit2Icon />
-            Ver meu GitHub
-          </Button>
-        </div>
+      <div className="flex flex-row flex-wrap justify-center gap-8">
+        <TitledSection level={3} title="Eu tenho vários projetos open source">
+          <div className="flex flex-col items-center">
+            <Button href="https://www.github.com/luizffgv/" target="_blank">
+              <FolderGit2Icon />
+              Ver meu GitHub
+            </Button>
+          </div>
+        </TitledSection>
+        <TitledSection level={3} title="E alguns pacotes NPM">
+          <div className="flex flex-col items-center">
+            <Button href="/projetos?tags=Pacote%20NPM" target="_blank">
+              <PackageIcon />
+              Ver meus pacotes
+            </Button>
+          </div>
+        </TitledSection>
       </div>
     </TitledSection>
   );
