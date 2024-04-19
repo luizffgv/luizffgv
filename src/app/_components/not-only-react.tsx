370958e75e9b72e27f213eade768414b479e5fa8
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-04-19 10:57:19 -0300

    Add non-react projects to the main page

--- src/app/_components/not-only-react.tsx
+++ src/app/_components/not-only-react.tsx
@@ -3,47 +3,76 @@ import Button from "./button";
 import GlowingText from "./glowing-text";
 import TitledSection from "./titled-section";
 import Tools from "./tools";
+import ProjectPreview from "./project-preview";
+import projects from "projects-list";
+
+const PROJECTS_WITHOUT_REACT = [
+  "Globe",
+  "Viewport-Based Scaling",
+  "HTilesML",
+  "Shader Site",
+] satisfies (typeof projects)[number]["name"][];
 
 export default function NotOnlyReact() {
   return (
     <>
       <TitledSection level={2} title="Mas eu não sei apenas React">
-        <div className="flex flex-col items-center gap-16">
+        <div className="flex flex-col items-center gap-32">
           <TitledSection
             level={3}
             title={
-              <span>
-                Eu tenho conhecimento nestes{" "}
-                <GlowingText>frameworks</GlowingText>,
-              </span>
+              <>
+                Veja projetos meus <GlowingText>sem</GlowingText> React
+              </>
             }
           >
-            <Tools kind="framework"></Tools>
+            <ul className="flex flex-row flex-wrap items-stretch justify-center gap-16 px-8">
+              {PROJECTS_WITHOUT_REACT.map((name) => (
+                <li key={name}>
+                  <ProjectPreview
+                    project={projects.find((p) => p.name === name)!}
+                  />
+                </li>
+              ))}
+            </ul>
           </TitledSection>
-          <TitledSection
-            level={3}
-            title={
-              <span>
-                nestas <GlowingText>linguagens</GlowingText>,
-              </span>
-            }
-          >
-            <Tools kind="language"></Tools>
-          </TitledSection>
-          <TitledSection
-            level={3}
-            title={
-              <span>
-                e nestas <GlowingText>ferramentas</GlowingText>.
-              </span>
-            }
-          >
-            <Tools kind="tool"></Tools>
-          </TitledSection>
-          <Button href="/projetos">
-            <EyeIcon />
-            Veja meus outros projetos
-          </Button>
+          <div className="flex flex-col items-center gap-16">
+            <TitledSection
+              level={3}
+              title={
+                <span>
+                  E tenho conhecimento nestes{" "}
+                  <GlowingText>frameworks</GlowingText>,
+                </span>
+              }
+            >
+              <Tools kind="framework"></Tools>
+            </TitledSection>
+            <TitledSection
+              level={3}
+              title={
+                <span>
+                  nestas <GlowingText>linguagens</GlowingText>,
+                </span>
+              }
+            >
+              <Tools kind="language"></Tools>
+            </TitledSection>
+            <TitledSection
+              level={3}
+              title={
+                <span>
+                  e nestas <GlowingText>ferramentas</GlowingText>.
+                </span>
+              }
+            >
+              <Tools kind="tool"></Tools>
+            </TitledSection>
+            <Button href="/projetos">
+              <EyeIcon />
+              Veja meus outros projetos
+            </Button>
+          </div>
         </div>
       </TitledSection>
     </>
