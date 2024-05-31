80bcc60ab50d5214054838b152349340a21ceaee
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-05-31 15:32:22 -0300

    Fix unicorn/prefer-ternary

--- src/app/_components/goo.tsx
+++ src/app/_components/goo.tsx
@@ -294,11 +294,7 @@ export class Bubbles {
 
   start(): void {
     this.#observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        this.#paused = false;
-      } else {
-        this.#paused = true;
-      }
+      this.#paused = !entries[0].isIntersecting;
     });
     this.#observer.observe(this.#element);
 
