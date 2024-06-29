9243c42f2508917b96c4b00d8545d3c8b2fe6adb
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-06-29 16:46:02 -0300

    Add hero exit animation

--- src/app/_components/hero/components/hero-advance-button/hero-advance-button.tsx
+++ src/app/_components/hero/components/hero-advance-button/hero-advance-button.tsx
@@ -7,7 +7,7 @@ export default function HeroAdvanceButton(): JSX.Element {
   return (
     <Button
       onClick={() => {
-        document.documentElement.scrollBy(0, innerHeight);
+        document.documentElement.scrollBy(0, innerHeight * 2);
       }}
       aria-label="Avançar"
     >
