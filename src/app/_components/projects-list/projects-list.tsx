5c1448dcb1ccb969219e8a495e482d846a1fab88
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-07-06 13:19:14 -0300

    New project list animations

--- src/app/_components/projects-list/projects-list.tsx
+++ src/app/_components/projects-list/projects-list.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useContext, useMemo } from "react";
 import { ProjectFilterContext } from "@/app/_contexts/project-filters";
 import ProjectPreview from "@components/project-preview";
@@ -9,11 +10,17 @@ export default function ProjectsList(): JSX.Element {
 
   return (
     <ul className="flex flex-row flex-wrap justify-center gap-8">
-      {filteredProjects.map((project) => (
-        <li key={project.name} className="flex max-w-md items-stretch">
-          <ProjectPreview project={project}></ProjectPreview>
-        </li>
-      ))}
+      <AnimatePresence>
+        {filteredProjects.map((project) => (
+          <motion.li
+            key={project.name}
+            className="min-w-fit grow basis-0 *:h-full"
+            layout
+          >
+            <ProjectPreview project={project}></ProjectPreview>
+          </motion.li>
+        ))}
+      </AnimatePresence>
       {filteredProjects.length === 0 && (
         <li className="text-center text-xl">
           Nenhum projeto se encaixa nos filtros.
